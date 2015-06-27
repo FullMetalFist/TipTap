969be33f949af313e345b5a2c42084af156791c0
Michael Vilabrera <m.vilabrera@gmail.com>
2015-06-27 12:33:20 -0400

    initial commit

---
+++ TipTap/TTNavController.swift
@@ -0,0 +1,35 @@
+//
+//  TTNavController.swift
+//  TipTap
+//
+//  Created by Michael Vilabrera on 6/27/15.
+//  Copyright (c) 2015 Giving Tree. All rights reserved.
+//
+
+import UIKit
+
+class TTNavController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TipTap/View/TTBillTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  TTBillTableViewCell.swift
+//  TipTap
+//
+//  Created by Michael Vilabrera on 6/27/15.
+//  Copyright (c) 2015 Giving Tree. All rights reserved.
+//
+
+import UIKit
+
+class TTBillTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+    }
+    
+    override func init(style: UITableViewCellStyle, reuseIdentifier: String!) {
+        
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
